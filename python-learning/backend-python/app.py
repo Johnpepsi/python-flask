30b54fb9dc05e4bd6fc3b539f022262ab060f609
John Palaganas <112512965+Johnpepsi@users.noreply.github.com>
2025-04-23 20:31:44 -0600

    Add files via upload

---
+++ python-learning/backend-python/app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/about')
+def about():
+    return "This is a simple Flask app!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
